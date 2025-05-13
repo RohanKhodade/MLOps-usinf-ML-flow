9816100665575fd3bcac12c6826900aed0831629
RohanKhodade <rohankhodade883@gmail.com>
2025-05-13 11:31:56 +0530

    local tracking and remote tracking using dagshub

---
+++ mlartifacts/157680559376718698/259cbc7496d141c1b684df04353b75fd/artifacts/file1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mlflow.set_tracking_uri("http://localhost:5000")
+# load wine dataset
+wine=load_wine()
+X=wine.data
+y=wine.target
+
+# train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.1,random_state=42)
+# define params for rf model
+max_depth=5
+n_estimators=10
+
+mlflow.set_experiment("mlops-2")
+with  mlflow.start_run():
+    rf=RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators,random_state=42)
+    rf.fit(X_train,y_train)
+    
+    y_pred=rf.predict(X_test)
+    accuracy=accuracy_score(y_pred,y_test)
+    
+    mlflow.log_metric("accuracy",accuracy)
+    mlflow.log_param("max_depth",max_depth)
+    mlflow.log_param("n_estimators",n_estimators)
+    
+    cm=confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(5,5))
+    sns.heatmap(cm,annot=True,xticklabels=wine.target_names,yticklabels=wine.target_names,fmt="d",cmap="Blues")
+    plt.ylabel("actual")
+    plt.xlabel("predicted")
+    plt.title("confusion matrix")
+    
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+    mlflow.log_artifact(__file__)
+    
+    print("accuracy",accuracy)
+    
